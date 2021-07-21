3eaaf966afabab9b0b10f3deec87116164fde4b1
Gabriel Mori Baleta <moribaleta.work@gmail.com>
2021-07-21 14:43:04 +0800

    auto layout

--- SampleApp/CounterViewController.swift
+++ SampleApp/CounterViewController.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+import UIKit
+
+
+public class CounterViewController : UIViewController {
+    
+    @IBOutlet weak var sampleview: SampleView!
+    @IBOutlet weak var button: UIButton!
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let random : UIColor = .random()
+        self.view.backgroundColor = random
+        
+        //sampleview.label.text = random.toHexString()
+        
+        let sample          = SampleView()
+        sample.label.text   = random.toHexString()
+        
+        self.view.addSubview(sample)
+        
+        sample.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
+        sample.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive       = true
+        sample.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive   = true
+        sample.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        sample.translatesAutoresizingMaskIntoConstraints = false
+        //self.view.setNeedsLayout()
+    
+    }
+    
+}
+
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(
+           red:   .random(),
+           green: .random(),
+           blue:  .random(),
+           alpha: 1.0
+        )
+    }
+    
+    func toHexString() -> String {
+                var r:CGFloat = 0
+                var g:CGFloat = 0
+                var b:CGFloat = 0
+                var a:CGFloat = 0
+
+                getRed(&r, green: &g, blue: &b, alpha: &a)
+
+                let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+
+                return String(format:"#%06x", rgb)
+            }
+}
--- SampleApp/SampleView.swift
+++ SampleApp/SampleView.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class SampleView: UIView {
-
+    
+    @IBOutlet var contentView: UIView!
+    
+    @IBOutlet weak var label: UILabel!
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -16,5 +19,25 @@ class SampleView: UIView {
         // Drawing code
     }
     */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed("SampleView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        //self.label.attributedText = NSAttributedString(string: "Sample Label")
+        self.label.text = "Sample Label"
+    }
 
 }
