e758aef2a8ffd701ce0707d18d43f4388a2edc03
Gabriel Mori Baleta <moribaleta.work@gmail.com>
2021-07-21 08:02:34 +0800

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Gabriel Mori Baleta on 7/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
