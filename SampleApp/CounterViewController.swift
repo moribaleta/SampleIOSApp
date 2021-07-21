740270d0e555579fd3038288a89536748f9a8148
Gabriel Mori Baleta <moribaleta.work@gmail.com>
2021-07-21 14:42:56 +0800

    auto layout

---
+++ SampleApp/CounterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CounterViewController.swift
+//  SampleApp
+//
+//  Created by Gabriel Mori Baleta on 7/21/21.
+//
+
+import Foundation
---
+++ SampleApp/SampleView.swift
@@ -0,0 +1,20 @@
+//
+//  SampleView.swift
+//  SampleApp
+//
+//  Created by Gabriel Mori Baleta on 7/21/21.
+//
+
+import UIKit
+
+class SampleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
